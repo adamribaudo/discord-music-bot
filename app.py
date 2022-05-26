692b35536eb2558af2c70e554f0a0c6f65637161
Adam Ribaudo <adam.ribaudo@velir.com>
2022-05-26 05:45:37 -0400

    updated to allow for multiple bots

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ import random
 from dotenv import load_dotenv, find_dotenv
 from os import listdir
 from os.path import isfile, join
-
-
+import sys
 
 if find_dotenv():
     load_dotenv()
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-PORT = os.getenv("PORT")
+
+BOT_PROFILE = sys.argv[1] # First argument should be an identifier for the bot. 
+# This will be used to pull the discord token, the voice channel name, and the music folder
+MUSIC_FOLDER = BOT_PROFILE
+VOICE_CHANNEL = BOT_PROFILE
+DISCORD_TOKEN = os.getenv(BOT_PROFILE)
 
 # Create bot
 client = commands.Bot(command_prefix='!')
@@ -25,18 +28,17 @@ async def on_ready():
     print('Bot ID: {}'.format(client.user.id))
  
 def create_audio_source():
-    all_tracks = [f for f in listdir('music') if isfile(join('music', f))]
+    all_tracks = [f for f in listdir(MUSIC_FOLDER) if isfile(join(MUSIC_FOLDER, f))]
     selected_track = random.choice(all_tracks)
     print("Selected " + selected_track)
-    #audio_source = PCMAudio(open(os.path.join('music',selected_track), mode='rb'))
-    audio_source = discord.FFmpegPCMAudio(source=os.path.join('music',selected_track),executable='ffmpeg')
+    audio_source = discord.FFmpegPCMAudio(source=os.path.join(MUSIC_FOLDER,selected_track),executable='ffmpeg')
     return audio_source
 
 @client.command(aliases=['next','paly', 'p', 'P', 'pap', 'pn', 'play_next', 'playnext'])
 async def play(ctx):
     guild = ctx.guild
     voice_client: discord.VoiceClient = guild.voice_client
-    if voice_client is None: voice_client = await discord.utils.get(guild.voice_channels, name='oooooooooooo').connect()
+    if voice_client is None: voice_client = await discord.utils.get(guild.voice_channels, name=VOICE_CHANNEL).connect()
 
     def after_play(error):
         if not voice_client.is_playing():
